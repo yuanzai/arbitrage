ff1e8d7b4b0ed66dc8fa617cf8e524eaa1ab8b55
yuanzai <junyuan.lau@gmail.com>
2017-07-01 17:49:32 -0700

    extract out csv related methods to its own class

--- src/com/arbitrage/CouponPullerTask.java
+++ src/com/arbitrage/CouponPullerTask.java
@@ -4,14 +4,9 @@
 
 package com.arbitrage;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.time.Clock;
-import java.time.LocalDateTime;
 import java.util.*;
 
-import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import org.apache.http.HttpEntity;
@@ -69,13 +64,13 @@ public class CouponPullerTask {
 
         ArbitrageAnalysis analysis = new ArbitrageAnalysis("CN", chinaCoinPrices, "US", usCoinPrices);
         ArbitrageAnalysisResult result = analysis.getResult();
-        String csvRow = getCsvRow(result, chinaCoinPrices, usCoinPrices);
+        String csvRow = CsvOutput.getCsvRow(result, chinaCoinPrices, usCoinPrices);
         logger.debug(csvRow);
         if (!result.validate()) {
             logger.error("Results cannot be validated. Not appending to data file");
         }
 
-        appendRowToFile("data.csv", csvRow);
+        CsvOutput.appendRowToFile("data.csv", csvRow);
 
         int minute = Calendar.getInstance().get(Calendar.MINUTE);
         if (minute == 10 || !diffResult.isEmpty()) {
@@ -98,50 +93,6 @@ public class CouponPullerTask {
             }
         }
         return diffPercentage;
-    }
-
-    private static String getCsvRow(ArbitrageAnalysisResult result, Map<String, Double> chinaCoinPrice, Map<String, Double> usCoinPrice) {
-        List<String> prices = new ArrayList<>();
-        for (String coin : COINS) {
-            Double cnPrice = chinaCoinPrice.get(coin);
-            Double usPrice = usCoinPrice.get(coin);
-            if (cnPrice != null) {
-                prices.add(cnPrice.toString());
-            } else {
-                prices.add("");
-            }
-            if (usPrice != null) {
-                prices.add(usPrice.toString());
-            } else {
-                prices.add("");
-            }
-        }
-        LocalDateTime dateTime = LocalDateTime.now(Clock.systemUTC());
-        return Joiner.on(",").join(dateTime, result, Joiner.on(",").join(prices));
-    }
-
-    private static String getCsvHeader() {
-        List<String> headers = new ArrayList<>();
-        headers.add("UTCDateTime");
-        headers.add("Market1");
-        headers.add("Buy1");
-        headers.add("BuyPrice1");
-        headers.add("Sell1");
-        headers.add("SellPrice1");
-
-        headers.add("Market2");
-        headers.add("Buy2");
-        headers.add("BuyPrice2");
-        headers.add("Sell2");
-        headers.add("SellPrice2");
-
-        headers.add("ArbitrageRatio");
-
-        for (String coin : COINS) {
-            headers.add(coin + "_CN");
-            headers.add(coin + "_US");
-        }
-        return Joiner.on(",").join(headers);
     }
 
     private String getDiffResult(Map<String, Double> coinChinaPrice, double rmb, Map<String, Double> usCoinPrice) {
@@ -241,23 +192,6 @@ public class CouponPullerTask {
         exchangeResponse.close();
         exchangeRateHttpClient.close();
         return rates.getDouble("CNY");
-    }
-
-    private static void appendRowToFile(String path, String row) throws IOException {
-        File file = new File(path);
-        if (!file.exists()) {
-            logger.debug("Creating file with headers at {}", path);
-            FileWriter fileWriter = new FileWriter(path, true);
-            fileWriter.append(getCsvHeader() + "\n");
-            fileWriter.flush();
-            fileWriter.close();
-        }
-        logger.debug("Append to {}", path);
-        FileWriter fileWriter = new FileWriter(path, true);
-        fileWriter.append(row + "\n");
-        fileWriter.flush();
-        fileWriter.close();
-
     }
 
 }
---
+++ src/com/arbitrage/CsvOutput.java
@@ -0,0 +1,85 @@
+package com.arbitrage;
+
+import com.google.common.base.Joiner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by junyuanlau on 1/7/17.
+ */
+public class CsvOutput {
+
+    private static final Logger logger = LoggerFactory.getLogger(CsvOutput.class);
+
+    private CsvOutput(){}
+
+
+    static void appendRowToFile(String path, String row) throws IOException {
+        File file = new File(path);
+        if (!file.exists()) {
+            logger.debug("Creating file with headers at {}", path);
+            FileWriter fileWriter = new FileWriter(path, true);
+            fileWriter.append(getCsvHeader() + "\n");
+            fileWriter.flush();
+            fileWriter.close();
+        }
+        logger.debug("Append to {}", path);
+        FileWriter fileWriter = new FileWriter(path, true);
+        fileWriter.append(row + "\n");
+        fileWriter.flush();
+        fileWriter.close();
+    }
+
+    private static String getCsvHeader() {
+        List<String> headers = new ArrayList<>();
+        headers.add("UTCDateTime");
+        headers.add("Market1");
+        headers.add("Buy1");
+        headers.add("BuyPrice1");
+        headers.add("Sell1");
+        headers.add("SellPrice1");
+
+        headers.add("Market2");
+        headers.add("Buy2");
+        headers.add("BuyPrice2");
+        headers.add("Sell2");
+        headers.add("SellPrice2");
+
+        headers.add("ArbitrageRatio");
+
+        for (String coin : CouponPullerTask.COINS) {
+            headers.add(coin + "_CN");
+            headers.add(coin + "_US");
+        }
+        return Joiner.on(",").join(headers);
+    }
+
+    static String getCsvRow(ArbitrageAnalysisResult result, Map<String, Double> chinaCoinPrice, Map<String, Double> usCoinPrice) {
+        List<String> prices = new ArrayList<>();
+        for (String coin : CouponPullerTask.COINS) {
+            Double cnPrice = chinaCoinPrice.get(coin);
+            Double usPrice = usCoinPrice.get(coin);
+            if (cnPrice != null) {
+                prices.add(cnPrice.toString());
+            } else {
+                prices.add("");
+            }
+            if (usPrice != null) {
+                prices.add(usPrice.toString());
+            } else {
+                prices.add("");
+            }
+        }
+        LocalDateTime dateTime = LocalDateTime.now(Clock.systemUTC());
+        return Joiner.on(",").join(dateTime, result, Joiner.on(",").join(prices));
+    }
+}
